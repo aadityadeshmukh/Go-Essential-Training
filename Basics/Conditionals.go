74b638bb07faf8ed9587e001c5f32bdf21772330
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2020-09-09 14:23:13 +0530

    conditional if and boolean operations

---
+++ Basics/Conditionals.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main()  {
+	a := 10
+	if a > 5{
+		fmt.Printf("a is greater")
+	}	
+
+	if a > 5 && a < 15{
+		fmt.Printf("a is just right")
+	}
+	
+	x := 11.0
+	b := 15.0
+	if frac:=x/b; frac > 0.5 {
+		fmt.Printf("x is greater than half of b")
+	}
+}	
