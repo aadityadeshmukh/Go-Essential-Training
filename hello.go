42aec7454e9b987f8beeff682105631f3ea8dea5
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2020-09-09 13:24:10 +0530

    1st commit

---
+++ hello.go
@@ -0,0 +1,17 @@
+//Prints a message
+//Anatomy of a Go program
+
+//define package main
+//This has a special meaning
+//This will compile the go program to an executable and not a package
+package main
+
+//import packages
+import (
+	"fmt"
+)
+
+// Define function
+func main(){
+	fmt.Println("Hello World!")
+}
