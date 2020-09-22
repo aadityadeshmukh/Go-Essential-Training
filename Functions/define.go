1fd3a7142a107f64107a816cfe250835d583c0cf
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2020-09-22 12:41:01 +0530

    pass by value

--- Functions/define.go
+++ Functions/define.go
@@ -11,10 +11,28 @@ func divmod(a int, b int) (int, int){
 	return a/b , a%b
 }
 
+func doubleAt(a[]int, i int) {
+	a[i] *= 2
+}
+
+func double(i int){
+	i *= 2
+}
+
 func main(){
 	val := add(7,10)
 	fmt.Println(val)
 
 	div, mod := divmod(7, 2)
-	fmt.Printf("\ndiv is %d and mod is %d" , div, mod)
+	fmt.Printf("div is %d and mod is %d\n" , div, mod)
+
+	values := [] int {1, 2, 4, 10}
+
+	doubleAt(values, 2)
+	fmt.Println(values)
+
+	num := 4
+	double(num)
+	fmt.Println(num)
+
 }
